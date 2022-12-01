3a2cd41bb084fd5b51b9fd4651d5a6e4e78e9d04
dpravos <dpravos@gmail.com>
2022-12-01 19:20:29 +0100

    [01:1] boilerplate

---
+++ src/test/java/com/dpravos/shared/ExampleInputGetter.java
@@ -0,0 +1,26 @@
+package com.dpravos.shared;
+
+public class ExampleInputGetter implements InputGetter {
+    @Override
+    public String day(int day) {
+        return switch (day) {
+            case 1 -> """
+                    1000
+                    2000
+                    3000
+                                            
+                    4000
+                                            
+                    5000
+                    6000
+                                            
+                    7000
+                    8000
+                    9000
+                                            
+                    10000
+                    """;
+            default -> throw new IllegalArgumentException("Invalid day number.");
+        };
+    }
+}
