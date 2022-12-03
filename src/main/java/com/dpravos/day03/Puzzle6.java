3b163d1d0db13b92e4a9288e12f2b3255ea9006e
dpravos <dpravos@gmail.com>
2022-12-03 18:37:29 +0100

    [03:2] solved example

--- src/main/java/com/dpravos/day03/Puzzle6.java
+++ src/main/java/com/dpravos/day03/Puzzle6.java
@@ -3,6 +3,8 @@ package com.dpravos.day03;
 import com.dpravos.shared.InputGetter;
 import com.dpravos.shared.Puzzle;
 
+import java.util.stream.IntStream;
+
 public class Puzzle6 extends Puzzle {
 
     public Puzzle6(InputGetter inputGetter) {
@@ -11,6 +13,15 @@ public class Puzzle6 extends Puzzle {
 
     @Override
     public String solve() {
-        return "70";
+        var lines = inputGetter.day(3).lines();
+
+        var groups = IntStream.range(0, lines.size() / 3)
+                .mapToObj(i -> lines.subList(i * 3, i * 3 + 3))
+                .map(linesGroup -> Group.fromContent(linesGroup.get(0), linesGroup.get(1), linesGroup.get(2)))
+                .toList();
+
+        Integer totalPriority = groups.stream().map(Group::priority).reduce(0, Integer::sum);
+
+        return String.valueOf(totalPriority);
     }
 }
