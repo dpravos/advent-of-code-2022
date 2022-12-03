26c6f620ec9c6d563fb8b117a853d0a3c4aa9cf5
dpravos <dpravos@gmail.com>
2022-12-03 18:26:59 +0100

    [03:2] refactor common type

--- src/main/java/com/dpravos/day03/Compartment.java
+++ src/main/java/com/dpravos/day03/Compartment.java
@@ -1,6 +1,5 @@
 package com.dpravos.day03;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,22 +11,22 @@ public class Compartment {
         this.items = Item.of(content);
     }
 
+    private List<ItemType> types() {
+        return items.stream()
+                .map(Item::type)
+                .toList();
+    }
+
     public String content() {
         return items.stream().map(Item::value).map(String::valueOf).collect(Collectors.joining());
     }
 
     public ItemType typeInCommonWith(Compartment that) {
-        List<ItemType> thisChars = this.content().chars()
-                .mapToObj(e -> new ItemType((char) e))
-                .toList();
-        List<ItemType> thatChars = that.content().chars()
-                .mapToObj(e -> new ItemType((char) e))
-                .toList();
-
-        var common = new ArrayList<>(thisChars);
-        common.retainAll(thatChars);
-
-        return common.get(0);
+        return this.types().stream()
+                .filter(that.types()::contains)
+                .distinct()
+                .toList()
+                .get(0);
     }
 
     public List<Item> items() {
