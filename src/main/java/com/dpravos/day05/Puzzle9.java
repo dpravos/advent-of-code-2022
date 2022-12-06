2b48d3678ca896a4bb9ea626c5a64825f4520234
dpravos <dpravos@gmail.com>
2022-12-06 19:40:07 +0100

    [05:1] solve example

--- src/main/java/com/dpravos/day05/Puzzle9.java
+++ src/main/java/com/dpravos/day05/Puzzle9.java
@@ -3,13 +3,32 @@ package com.dpravos.day05;
 import com.dpravos.shared.InputGetter;
 import com.dpravos.shared.Puzzle;
 
+import java.util.Arrays;
+
 public class Puzzle9 extends Puzzle {
+
+    private final InputParser inputParser = new InputParser();
+    private final CargoParser cargoParser = new CargoParser();
+    private final InstructionsParser instructionsParser = new InstructionsParser();
     public Puzzle9(InputGetter inputGetter) {
         super(inputGetter);
     }
 
     @Override
     public String solve() {
-        return "CMZ";
+
+        var input = inputGetter.day(5);
+
+        var cargoInput = inputParser.cargoInput(input);
+        var instructionsInput = inputParser.instructionsInput(input);
+
+        var stacks = cargoParser.parse(cargoInput);
+        var instructions = instructionsParser.parse(instructionsInput);
+
+        var crane = new Crane(stacks);
+
+        instructions.forEach(crane::move);
+
+        return String.join("", Arrays.stream(stacks).map(Stack::top).toList());
     }
 }
